e39de6d3a0a759408bd94b9629462b682ad9e28c
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-04-06 02:59:08 +0200

    Add safe error in case optional migrations don't work

--- migrations/20210401_000400_create_product_term_table.php
+++ migrations/20210401_000400_create_product_term_table.php
@@ -9,6 +9,12 @@ return [
         return $schema->hasTable('flamarkt_products');
     },
     'up' => function (Builder $schema) {
+        // Fallback message in case there are still issues with optional migrations in flamarkt/backoffice
+        // This way the users see a better error instead of a broken database
+        if (!$schema->hasTable('flamarkt_products')) {
+            throw new \Exception('flamarkt_products table is missing. This code should not have run. This could be an issue with flamarkt/backoffice');
+        }
+
         $schema->create('flamarkt_product_taxonomy_term', function (Blueprint $table) {
             $table->unsignedInteger('product_id');
             $table->unsignedInteger('term_id');
